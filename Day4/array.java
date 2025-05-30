f123ea864d325acd560a3fd38db20be235889c4f
Sudharshanreddy18 <levakusudharshanreddy@gmail.com>
2025-05-30 11:10:18 +0530

    first commit

---
+++ Day4/array.java
@@ -0,0 +1,19 @@
+public class array {
+    public static void main(String[] args) {
+        int[] ECE_F = new int[10];
+        ECE_F[0] = 5019;
+        ECE_F[1] = 5011;
+        ECE_F[2] = 5016;
+        ECE_F[3] = 5013;
+        ECE_F[4] = 5012;
+        ECE_F[5] = 5010;
+        System.out.println("roll no:" +ECE_F[0]);
+       System.out.println("roll no:" +ECE_F[1]);
+       System.out.println("roll no:" +ECE_F[2]);
+       System.out.println("roll no:" +ECE_F[3]);
+       System.out.println("roll no:" +ECE_F[4]);
+       System.out.println("roll no:" +ECE_F[5]);
+       
+    }
+    
+}
