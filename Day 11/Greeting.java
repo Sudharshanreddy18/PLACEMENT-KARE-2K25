dfac6a29fce3081a16fe3e15c4765ab3a3b6e035
Sudharshanreddy18 <levakusudharshanreddy@gmail.com>
2025-06-07 15:20:58 +0530

    Delete Day 11 directory

--- Day 11/Greeting.java
+++
@@ -1,9 +0,0 @@
-public class Greeting {
-    public static void printGreeting(String name) {
-        System.out.println("Hello, " + name + "!");
-    }
-
-    public static void main(String[] args) {
-        printGreeting("Alice");
-    }
-}
